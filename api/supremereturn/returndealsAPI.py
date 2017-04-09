786e2a1da18cfcdba54587a301d93b8b26650c45
thepiedpiper29 <rohit.rk.rk1@gmail.com>
2017-04-10 02:20:58 +0530

    Add files via upload

---
+++ api/supremereturn/returndealsAPI.py
@@ -0,0 +1,8 @@
+import csv
+datafile = open(r'C:\Users\sskaushik\Desktop\hoolixyz\dealsdata.csv', 'r')
+datareader = csv.reader(datafile,delimiter=';')
+data = []
+for row in datareader:
+    data.append(row)    
+print (data[1:300])
+		
