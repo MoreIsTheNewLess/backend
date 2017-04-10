132f290878b631410f6400d64a756131faa23338
thepiedpiper29 <rohit.rk.rk1@gmail.com>
2017-04-10 22:11:23 +0530

    Delete returndealsAPI.py

--- api/supremereturn/returndealsAPI.py
+++
@@ -1,8 +0,0 @@
-import csv
-datafile = open(r'C:\Users\sskaushik\Desktop\hoolixyz\dealsdata.csv', 'r')
-datareader = csv.reader(datafile,delimiter=';')
-data = []
-for row in datareader:
-    data.append(row)    
-print (data[1:300])
-		
