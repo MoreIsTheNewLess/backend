78e9e5c5d8d0897e619cf94d77984278caeccd9f
Sireesh Kodali <sireeshkodali1@gmail.com>
2017-04-15 16:54:19 +0530

    Actually do deals

--- api/deals/views.py
+++ api/deals/views.py
@@ -5,10 +5,10 @@ from rest_framework import status
 import urllib.request, json
 from sklearn import svm
 from sklearn.model_selection import train_test_split
+import csv
 
 
-
-clf = svm.SVC(gamma=0.001, C=100.,probability=True)
+clf = svm.SVC(gamma = 0.001, C = 100., probability = True)
 data = []
 target = []
 
@@ -18,33 +18,36 @@ test_target = []
 print("generating random test data...")
 for p in range(1,4):
     if p == 1:
+        prdt = "UX" #TODO : Don't do this
         i = 7
     elif p == 2:
+        prdt = "LB" #TODO : Don't do this
         i = 14
     elif p == 3:
+        prdt = "MW" #TODO : Don't do this
         i = 28
     for j in range(1,13):
-        temp = [int(i),int(j)]
+        temp = [int(i), int(j)]
         if j % 2 == 0:
             data.append(temp)
-            target.append(int(p))
+            target.append(prdt)
         else:
             test_data.append(temp)
-            test_target.append(int(p))
-        temp = [int(i) + 1,int(j)]
+            test_target.append(prdt)
+        temp = [int(i) + 1, int(j)]
         if j % 2 == 0:
             data.append(temp)
-            target.append(int(p))
+            target.append(prdt)
         else:
             test_data.append(temp)
-            test_target.append(int(p))
-        temp = [int(i) - 1,int(j)]
+            test_target.append(prdt)
+        temp = [int(i) - 1, int(j)]
         if j % 2 == 0:
             data.append(temp)
-            target.append(int(p))
+            target.append(prdt)
         else:
             test_data.append(temp)
-            test_target.append(int(p))
+            test_target.append(prdt)
 
 print("training...")
 clf.fit(data,target)
@@ -54,7 +57,27 @@ print(clf.score(test_data, test_target))
 
 class deals(APIView):
     def get(self, request, *args, **kw):
-        d = int(request.GET.get("date"))
-        m = int(request.GET.get("month"))
-        l = clf.predict_proba([d,m]).tolist()
-        return Response(json.dumps(l[:5]), status=status.HTTP_200_OK)
+        secret_path = '/Users/sireesh/Documents/School/12A/Computers/'  #NOTE: Change to the path of backend folder on your computer
+        day = int(request.GET.get("day"))
+        month = int(request.GET.get("month"))
+        BuyNLargeList = clf.predict_proba([day, month]).tolist()
+        bestProduct = 0
+
+        categories = { 'UX' : 'Groceries', 'LB' : 'Electricity', 'ML' : 'Fuel' }
+
+        if BuyNLargeList[0][0] > BuyNLargeList[0][1] and BuyNLargeList[0][0] > BuyNLargeList[0][2]:#TODO: Better product matching
+            bestProduct = 'UX'
+        elif BuyNLargeList[0][1] > BuyNLargeList[0][2]:
+            bestProduct = 'LB'
+        else:
+            bestProduct = 'ML'
+
+        less = ''
+        with open(secret_path + 'backend/api/supremereturn/SupremeDeals.csv', newline = '') as csvfile:
+            deals = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+            for row in deals:
+                if bestProduct in row[-1]:
+                    less = row
+                    break
+
+        return Response({'Reminder!' : bestProduct, 'Best Deals' : less}, status = status.HTTP_200_OK)
